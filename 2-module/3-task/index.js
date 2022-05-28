351e2f3ebe4131eeb495305565e2daa29f0b1579
Aleksey Chaplin <lexxxach@yandex.ru>
2022-05-28 10:47:55 +0500

    описана функция алькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+  read: '',
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
+
+let read = function read(a, b) {
+  this.a = a;
+  this.b = b;
+};
+
+calculator.read = read;
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
