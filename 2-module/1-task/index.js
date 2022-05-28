2b0c9d18f5be3f77336a997d8876550f30fc0154
Aleksey Chaplin <lexxxach@yandex.ru>
2022-05-28 09:48:48 +0500

    доработана функция расчета зарплаты

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,41 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+
+  let objLength = Object.keys(salaries).length;
+
+  if (objLength) {
+    for (key in salaries) {
+      if (typeof salaries[key] == 'number') {
+        result = result + checkRes(salaries[key]);
+
+        console.log(result);
+      }
+    }
+  }
+  
+  return result
+  
+}
+
+/*проверка на NaN, -Infinity, Infinity
+	- возврат 0 или переданное значение
+*/
+function checkRes(res) {
+  let result = 0;
+  if (!Number.isNaN(res)) {
+    switch (res) {
+      case Infinity:
+        break;
+
+      case -Infinity:
+        break;
+
+      default:
+        result = res;
+
+        break;
+    }
+  }
+
+  return result;
 }
