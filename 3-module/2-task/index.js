289af6522ea53448bc6542b7781cb8da6a9db0fc
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-06 01:07:07 +0500

    описана функция модуль 3 задача 2_исправление1

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,8 +1,8 @@
 function filterRange(arr, a, b) {
-  let copyArr = arr.slice();
-  console.log(copyArr);
+  //let copyArr = arr.slice();
+  //console.log(copyArr);
 
-  let result = copyArr.filter((item) => item >= 1 && item <= b);
+  let result = arr.filter((item) => item >= a && item <= b);
 
   return result;
 }
