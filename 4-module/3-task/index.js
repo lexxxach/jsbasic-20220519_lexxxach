5e04c7860a6e0603f69d9fbb8be64539694120df
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-05 12:18:23 +0500

    описана функция модуль 4 задача 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,70 @@
 function highlight(table) {
-  // ваш код...
+  //строки со значениями
+  let rows = table.querySelectorAll('tbody tr')
+
+  for (i = 0; i < rows.length; i++) {
+    
+    let ageControl = 18
+    let currentRow = rows[i]
+    let elAge = rows[i].cells[1]
+    let elGender = rows[i].cells[2]
+    let elStatus = rows[i].cells[3]
+
+    let propAge = (age, ageControl) => {
+
+      let res
+
+      if (age < ageControl) {
+        res = 'line-through'
+      }
+
+      return res
+
+    }
+    
+    let propGender = (gender) => {
+
+      let res
+
+      if (gender == 'm') {
+        res = 'male'
+      } else if (gender == 'f') {
+        res = 'female'
+      }
+
+      return res
+
+    }
+    
+    let propStatus = (status) => {
+
+      let res
+
+      if (status) {
+        res = 'available'
+      } else {
+        res = 'unavailable'
+      }
+
+      return res
+
+    }
+
+    //обработка возраст
+    if (+elAge.textContent < ageControl) {
+      currentRow.style['text-decoration'] = propAge(Number(elAge.textContent), ageControl)
+    }
+
+    //обработка статус
+    if (elStatus.dataset.available) {
+      currentRow.className = propStatus(elStatus.dataset.available)
+    } else {
+      currentRow.setAttribute('hidden', true)
+    }
+
+    //обработка пол
+    currentRow.classList.add(propGender(elGender.textContent))
+
+  }
+
 }
