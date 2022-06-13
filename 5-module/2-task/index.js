c84e30001168d499628e75924c6fe8e510dfe046
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-13 18:10:24 +0500

    Блок 5_Задание 2_1

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,13 +1,14 @@
 function toggleText() {
-  let elText = document.getElementById('text')
-  let attrName = 'hidden'
-  if(elText.hasAttribute(attrName)){
-    elText.removeAttribute(attrName)
-  } else {
-    elText.setAttribute(attrName,'')
-  }
+
+  let elBtn = document.getElementsByClassName('toggle-text-button')[0]
+
+  elBtn.addEventListener('click', function () {
+    let elText = document.getElementById('text')
+    let attrName = 'hidden'
+    if (elText.hasAttribute(attrName)) {
+      elText.removeAttribute(attrName)
+    } else {
+      elText.setAttribute(attrName, '')
+    }
+  })
 }
-
-let elBtn = document.getElementsByClassName('toggle-text-button')[0]
-
-elBtn.addEventListener('click',toggleText)
