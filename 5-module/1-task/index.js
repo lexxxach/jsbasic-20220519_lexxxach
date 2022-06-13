5913ada251491528169f0819d9cbca5638b9025b
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-13 17:58:57 +0500

    Блок 5_Задание 2

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,9 +1,8 @@
 function hideSelf() {
-  
+
   let elButt = document.getElementsByClassName('hide-self-button')[0]
-  
-  elButt.addEventListener('click',function(){
-    this.setAttribute('hidden','')
+  elButt.addEventListener('click', function () {
+    this.setAttribute('hidden', '')
   })
 
 }
