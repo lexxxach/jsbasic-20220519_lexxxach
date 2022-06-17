f5d8abb0f8cd7bfdcd3ab92aff16b40c87cd08db
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-18 00:29:53 +0500

    Блок 6_Задание_1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,55 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.#createTable(rows)
+
+  }
+
+  #createTable(rows) {
+    let elemTable = document.createElement('TABLE')
+    elemTable.innerHTML = `
+    <thead>
+    <tr>
+      <th>Имя</th>
+      <th>Возраст</th>
+      <th>Зарплата</th>
+      <th>Город</th>
+      <th></th>
+    </tr>
+  </thead>
+    `
+    this.#addRows(elemTable, rows)
+
+    return elemTable
+
+  }
+
+  /* Добавление строк */
+  #addRows(elemTable, rows) {
+    
+    if (rows) {
+      
+      let elBody = document.createElement('TBODY')
+
+      for (let el of rows) {
+        let elTr = document.createElement('TR')
+        elTr.innerHTML = `
+        <td>${el.name}</td>
+            <td>${el.age}</td>
+            <td>${el.salary}</td>
+            <td>${el.city}</td>
+            <td><button>X</button></td>
+        `
+        let btn = elTr.querySelector('button')
+        
+        btn.addEventListener('click', function () {
+          this.parentElement.parentElement.remove()
+        })
+
+        elBody.append(elTr)
+      }
+
+      elemTable.append(elBody)
+    }
   }
 }
