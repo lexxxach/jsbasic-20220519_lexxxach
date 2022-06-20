68efe326c3fd5864a06c0b42f7fa1dcf75c3f48e
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-20 10:37:07 +0500

    Блок 6_Задание_3_3

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -18,16 +18,7 @@ export default class Carousel {
     <div class="carousel__arrow carousel__arrow_left">
       <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
     </div>
-
-    
     ${this.#elemsInner()}
-    
-    
-    
-    
-    
-      
-    
   </div>`
     )
 
@@ -36,44 +27,35 @@ export default class Carousel {
     for (let slide of this.slides) {
 
       let elDiv = elem.querySelector(`[data-id="${slide.id}"]`)
-     
-     let btn = elDiv.getElementsByClassName('carousel__button')[0]
+      let btn = elDiv.getElementsByClassName('carousel__button')[0]
 
-     btn.addEventListener('click',function(){
+      btn.addEventListener('click', function () {
+        let userEvent = new CustomEvent('product-add', {
+          detail: slide.id,
+          bubbles: true
+        })
 
-      let userEvent = new CustomEvent('product-add',{
-        detail: slide.id,
-        bubbles:true
+        btn.dispatchEvent(userEvent)
       })
-
-      btn.dispatchEvent(userEvent)
-
-
-     })
-
-
     }
 
-
-      
-
-
+    this.#initCarousel(elem)
 
     return elem
   }
 
-  #elemsInner(){
+  #elemsInner() {
 
     let elemCarousel = createElement(
-     `
+      `
      <div class="carousel__inner">
     </div>
       `)
- 
-       for (let slide of this.slides){
 
-        let elSlide = createElement(
-          `<div class="carousel__slide" data-id="${slide.id}">
+    for (let slide of this.slides) {
+
+      let elSlide = createElement(
+        `<div class="carousel__slide" data-id="${slide.id}">
           <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
           <div class="carousel__caption">
           <span class="carousel__price">€${slide.price.toFixed(2)}<!--значение slide.price--></span>
@@ -84,19 +66,55 @@ export default class Carousel {
           </div>
           </div>
          `
-          ) 
+      )
 
-        
+      elemCarousel.append(elSlide)
+    }
+    return elemCarousel.outerHTML
+  }
+
+  #initCarousel(elem){
+    let elBtnL = elem.getElementsByClassName('carousel__arrow carousel__arrow_left')[0]
+    let elBtnR = elem.getElementsByClassName('carousel__arrow carousel__arrow_right')[0]
+  
+    let chClick = 0
+    checkShow(chClick, elBtnR, elBtnL)
+  
+    elBtnL.addEventListener('click', () => {
+      if (chClick > 0) {
+        let elCarousel = elem.getElementsByClassName('carousel__inner')[0]
+        chClick -= 1
+        elCarousel.style.transform = `translateX(-${chClick * elCarousel.offsetWidth}px  )`
+        checkShow(chClick, elBtnR, elBtnL)
+      }
+    })
+  
+    elBtnR.addEventListener('click', () => {
+      if (chClick < 3) {
+        chClick += 1
+        let elCarousel = elem.getElementsByClassName('carousel__inner')[0]
+        elCarousel.style.transform = `translateX(-${chClick * elCarousel.offsetWidth}px  )`
+        checkShow(chClick, elBtnR, elBtnL)
+      }
+    })
+  
+    function checkShow(chClick, elR, elL) {
+      switch (chClick) {
+        case 0:
+          elL.style.display = 'none'
+          break
+  
+        case 3:
+          elR.style.display = 'none'
+          break
+        default:
+          elR.style.display = ''
+          elL.style.display = ''
+          break
+      }
+    }
 
 
-        elemCarousel.append(elSlide)
-      
-       /*  let btn = elSlide.querySelector('BUTTON')
-        alert(btn)
-        btn.onclick= ()=>alert(2323) */
-
-      } 
-      return elemCarousel.outerHTML
   }
 
 
