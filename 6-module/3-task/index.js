8e9a8638c6820ce3cb74e8bab3c97a2e42a063ac
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-20 08:50:22 +0500

    Блок 6_Задание_2_1

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,57 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.elem = this.#createElem()
   }
+
+  #createElem() {
+
+    let elem = createElement(
+      ` <!--Корневой элемент карусели-->
+  <div class="carousel">
+    <!--Кнопки переключения-->
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+    </div>
+
+    
+${this.#elemsInner()}      
+    
+  </div>`
+    )
+    return elem
+  }
+
+  #elemsInner(){
+
+    let elemCarousel = createElement(
+     `
+     <div class="carousel__inner">
+    </div>
+      `)
+ 
+       for (let slide of this.slides){
+
+        let elSlide = createElement(
+          `<div class="carousel__slide" data-id="penang-shrimp">
+          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+          <span class="carousel__price">€${slide.price.toFixed(2)}<!--значение slide.price--></span>
+          <div class="carousel__title">${slide.name}</div>
+          <button type="button" class="carousel__button">
+          <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+          </div>
+          </div>
+         `
+          ) 
+        elemCarousel.append(elSlide)
+      } 
+      return elemCarousel.outerHTML
+  }
+
+
 }
