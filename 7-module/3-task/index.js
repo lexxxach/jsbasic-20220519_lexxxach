f0ca349b08a8a4bc792d4a8dff896bd992e37a95
Aleksey Chaplin <lexxxach@yandex.ru>
2022-06-29 08:08:12 +0500

    Блок 7_Задание_3_7

--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,7 +1,7 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class StepSlider {
-  #elemInner = ''
+
   constructor({ steps, value = 0 }) {
     this.elem = this.#innerElem(steps)
   }
@@ -11,44 +11,38 @@ export default class StepSlider {
     let elemBefore = createElement(
       `
       <!--Корневой элемент слайдера-->
-  <div class="slider">
+      <div class="slider">
 
-    <!--Ползунок слайдера с активным значением-->
-    <div class="slider__thumb" style="left: 0%;">
+      <!--Ползунок слайдера с активным значением-->
+      <div class="slider__thumb" style="left: 0%;">
       <span class="slider__value">0</span>
-    </div>
+      </div>
 
-    <!--Заполненная часть слайдера-->
-    <div class="slider__progress" style="width: 0%;"></div>
+      <!--Заполненная часть слайдера-->
+      <div class="slider__progress" style="width: 0%;"></div>
 
-    <!--Шаги слайдера-->
-    <div class="slider__steps">
+      <!--Шаги слайдера-->
+      <div class="slider__steps">
       <span></span>
       <span></span>
       <span class="slider__step-active"></span>
       <span></span>
       <span></span>
-    </div>
-  </div>
+      </div>
+      </div>
       `
     )
 
+    /* Добавление SPAN по количеству шагов */
     let elemSteps = elemBefore.querySelector('.slider__steps')
+    elemSteps.innerHTML = ''
 
-    getElSpan(steps, elemSteps)
-
-    function getElSpan(stepsFunc, el) {
-
-      el.innerHTML = ''
-
-      for (let i = 0; i < stepsFunc; i++) {
-        let elemStep = document.createElement('SPAN')
-        if(i==0){
-          elemStep.classList.add('slider__step-active')
-        }
-        el.append(elemStep)
+    for (let i = 0; i < steps; i++) {
+      let elemStep = document.createElement('SPAN')
+      if (i == 0) {
+        elemStep.classList.add('slider__step-active')
       }
-
+      elemSteps.append(elemStep)
     }
 
     /* Обработка событий клика */
@@ -56,47 +50,81 @@ export default class StepSlider {
 
     function coordProc(event) {
 
-      let box = this.getBoundingClientRect()
+      let numberStep = getNumberStep(this, event) //номер шага
+     
+      /* Установка значения шага в SPAN */
+      let elemSliderValue = document.querySelector('.slider__value')
+      let sliderValueBefore = elemSliderValue.textContent
+      elemSliderValue.textContent = numberStep
+      let sliderValueAfter = elemSliderValue.textContent
+
+      /* Условие подписки на событие */
+      if (!(sliderValueBefore == sliderValueAfter)) {
+        let custEvent = new CustomEvent('slider-change', {
+          detail: +sliderValueAfter,
+          bubbles: true
+        }
+        )
+        elemBefore.dispatchEvent(custEvent)
+      }
+
+      let elemsSpanSliderArr = document.querySelectorAll('.slider__steps SPAN')
+      /* Обработка активного SPAN */
+      for (let el of elemsSpanSliderArr) {
+        el.removeAttribute('class')
+      }
+      elemsSpanSliderArr[numberStep].classList.add('slider__step-active')
+
+      let progress = numberStep * 100 / (steps - 1) //значение номера шага в %
+
+      /* Обрабокта перемещения позунка */
+      let elThumb = elemBefore.querySelector('.slider__thumb')
+      elThumb.style.left = `${progress}%`
+
+      /* Обработка ширины закрашиваемой области */
+      let elThumbArea = elemBefore.querySelector('.slider__progress')
+      elThumbArea.style.width = `${progress}%`
+
+    }
+
+    //Вычисление значения ближайшего шага
+    let getNumberStep = (elBtn, eventBtn) => {
+
+      let box = elBtn.getBoundingClientRect()
       let elLeft = box.left //левый угол
       let elRight = box.right //правый угол
       let elWidth = elRight - elLeft //длина элемента
-      let elClick = event.clientX //координаты клика
+      let elClick = eventBtn.clientX //координаты клика
       let stepLength = elWidth / (steps - 1) //длина шага
-      
+      let arrCoord = [] //массив для ближайших координат
+      let arrSteps = [] //массив для шагов
 
-      let arrCoord = []
-      let arrSteps = []
-
-      let arrStep = 0
+      let arrStep = 0 //счеттчик шага
       for (let i = elLeft; i <= elRight; i = i + stepLength) {
         let delta1 = elClick - i
-
+        
         if (Math.abs(delta1) < stepLength) {
           arrCoord.push(i)
         }
+
         arrSteps.push(arrStep)
-
         arrStep++
-
       }
 
-      let xCoord1 = Math.abs(arrCoord[0] - elClick)
-      let xCoord2 = Math.abs(arrCoord[1] - elClick)
-      let delta2 = Math.min(xCoord1, xCoord2)
-      let sliderCoord
-
+      let xCoord1 = Math.abs(arrCoord[0] - elClick) //дельта между кликом и левой кординатой
+      let xCoord2 = Math.abs(arrCoord[1] - elClick) //дельта между кликом и правой кординатой
+      let delta2 = Math.min(xCoord1, xCoord2) //минимальное расстояние между кликом и координатой
+     
+      let sliderCoord //координата ближайшего шага
       if (delta2 == xCoord1) {
         sliderCoord = arrCoord[0]
       } else if (delta2 == xCoord2) {
         sliderCoord = arrCoord[1]
       }
 
-      //alert(sliderCoord)
-
+      /* Вычисление шага */
       let numberStep = 0
       for (let count of arrSteps) {
-
-
 
         if ((Math.abs(sliderCoord - (elLeft + count * stepLength))) < 1) {
           numberStep = count
@@ -105,69 +133,11 @@ export default class StepSlider {
 
       }
 
-      let elemSliderValue = document.querySelector('.slider__value')
-      let sliderValueBefore = elemSliderValue.textContent
-      elemSliderValue.textContent = numberStep
-      let sliderValueAfter = elemSliderValue.textContent
-
-      if(!(sliderValueBefore == sliderValueAfter)){
-        let custEvent = new CustomEvent('slider-change',{
-        detail:+sliderValueAfter,
-        bubbles:true}
-        )
-        elemBefore.dispatchEvent(custEvent)     
-
-      }
-
-      
-
-      let elemsSpanSliderArr = document.querySelectorAll('.slider__steps SPAN')
-
-      for (let el of elemsSpanSliderArr) {
-        el.removeAttribute('class')
-      }
-      
-      
-      elemsSpanSliderArr[numberStep].classList.add('slider__step-active')
-      
-        let progress = numberStep * 100 / (steps - 1)
-
-        /* Обрабокта перемещения позунка */
-      let elThumb = elemBefore.querySelector('.slider__thumb')
-      elThumb.style.left = `${progress}%`
-
-      /* Обработка ширины закрашиваемой области */
-      let elThumbArea = elemBefore.querySelector('.slider__progress')
-      elThumbArea.style.width = `${progress}%`
-        
-
-
+      return numberStep
     }
 
-    
-
-
-
-
-
-
     return elemBefore
-
   }
-
-
-  #getCoord(elSlider) {
-    let sliderX
-
-    return sliderX
-  }
-
-
-
-
-
-
-
 
 }
 
