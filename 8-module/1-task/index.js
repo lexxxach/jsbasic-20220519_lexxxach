6427222385cdc5460809e192135c146a3b79cf0a
Aleksey Chaplin <lexxxach@yandex.ru>
2022-07-02 22:39:43 +0500

    Блок 8_Задание_1_1

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,8 +1,10 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class CartIcon {
+  #offSet = 0
   constructor() {
     this.render();
+    this.#offSet = this.elem.getBoundingClientRect().top - window.pageYOffset
 
     this.addEventListeners();
   }
@@ -26,7 +28,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +41,45 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    let el = this.elem
+    
+    if (this.#offSet < window.pageYOffset) {
+
+      if (document.documentElement.clientWidth > 767) {
+
+        el.style.position = 'fixed'
+        let elContainer = document.querySelector('.container')
+        let coordRightBorder = elContainer.getBoundingClientRect().right //правая часть элемента
+        let windowWidth = document.documentElement.clientWidth //ширина окна
+
+        if (coordRightBorder + 20 <= windowWidth - el.offsetWidth - 10) {
+          el.style.left = coordRightBorder + 20 + 'px'
+        }
+        else {
+          el.style.left = windowWidth - el.offsetWidth - 10 + 'px'
+          el.style.zIndex = 1000
+        }
+      }
+      else {
+
+        Object.assign(this.elem.style,{
+          position: '',
+          top: '',
+          left: '',
+          zIndex: ''
+        })
+        
+      }
+    }else
+    {
+      Object.assign(this.elem.style,{
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      })
+
+    }
+
   }
 }
