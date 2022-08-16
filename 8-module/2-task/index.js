2f6e07cd9dece94f4c65c23b5fee245b4028c00a
Aleksey Chaplin <lexxxach@yandex.ru>
2022-08-16 09:30:51 +0500

    Блок 8_Задание_2_1

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,37 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.elem = this.#render(products)
   }
+
+  #render(products) {
+    let elem = createElement(`
+    <div class="products-grid">
+    <div class="products-grid__inner">
+      ${this.#innerElem(products)}
+    </div>
+    </div>
+    `)
+
+    return elem
+
+  }
+
+  #innerElem(products) {
+    
+    let innerProductDiv = document.createElement('DIV')
+    for (let currentProduct of products) {
+      let elemCard = new ProductCard(currentProduct)
+      innerProductDiv.append(elemCard.elem)
+    }
+
+    return innerProductDiv.innerHTML
+  }
+
+  updateFilter(filters){
+    alert(111)
+
+  }
+
 }
+
